662854f7525ba591da2cfcdcd89c51afb2a68190
Andrew Butson <butsona@arbs.io>
2023-10-06 20:53:44 +0200

    - Modified webview-ui/src/App.tsx to remove a commented out function and replace it with a new implementation. - Removed console.log statements from the App function. - Removed the call to processInput function when element.claimValue is not an array.

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -8,40 +8,10 @@ import {
 import { ITokenListItem, tokenListItems } from './utilities/tokenListItems'
 import './App.css'
 
-// function processInput(input: string): string {
-//   try {
-//     // Try to parse the input as JSON
-//     const arr = JSON.parse(input)
-
-//     // Check if the parsed input is an array
-//     if (Array.isArray(arr)) {
-//       // Join the array elements with a newline and return
-//       return arr.join('zzzzz\n')
-//     }
-//   } catch (error) {
-//     // If an error is thrown, the input is not a valid JSON array
-//     // Just ignore the error
-//   }
-
-//   // If the input is not a JSON array, return it as is
-//   return input
-// }
-function processInput(input: string): string {
-  // Check if the input looks like a JSON array
-  if (/^\s*\[.*\]\s*$/.test(input)) {
-    // If it does, parse it as JSON and join the elements with a newline
-    return JSON.parse(input).join('aaaaaaaaa\n')
-  }
-
-  // If the input does not look like a JSON array, return it as is
-  return input
-}
-
 function App() {
   const [state, setState] = useState<MessageEvent>()
   const onMessageReceivedFromIframe = useCallback(
     (event: MessageEvent) => {
-      console.log('onMessageReceivedFromIframe', event)
       setState(event)
     },
     [state]
@@ -60,17 +30,12 @@ function App() {
     )
     tempTokenData.forEach((element) => {
       if (element.claimValue) {
-        console.log('element.claimName', element.claimName)
-        console.log('element.claimName', element.claimValue)
-        console.log('element.isArray', Array.isArray(element.claimValue))
         if (Array.isArray(element.claimValue)) {
           let stringArray = ''
           element.claimValue.sort().forEach((claimItem) => {
             stringArray = stringArray + `🏷️ ${claimItem}\n`
           })
           element.claimValue = stringArray
-        } else {
-          element.claimValue = processInput(element.claimValue)
         }
       }
       tokenData.push(element)
