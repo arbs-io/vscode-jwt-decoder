0eb66ae3cb35508f34761acf0a5dcaa1b3f86503
Andrew Butson <butsona@arbs.io>
2023-10-06 21:41:03 +0200

    - Modified .vscode/launch.json to simplify the "args" array and change the "preLaunchTask" from "npm: watch" to "npm: esbuild-watch" - Deleted the "Run Extension - esbuild-base" and "Run Extension Tests" configurations from .vscode/launch.json - Modified .vscode/tasks.json to replace the "watch" task with a new "npm: esbuild-watch" task, and added a problemMatcher for TypeScript - Added utilities/scripts/clean.js to delete certain directories if they exist

---
+++ utilities/scripts/clean.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const folders = [
+  '../../dist',
+  '../../out',
+  '../../node_modules',
+  '../../webview-ui/node_modules',
+]
+
+folders.forEach((folder) => {
+  const dir = path.join(__dirname, folder)
+  if (fs.existsSync(dir)) {
+    fs.rmSync(dir, { recursive: true, force: true })
+  }
+})
